7a8c1afdabb6d74ae660ccfe8ba2df91183198fe
Luke Drury <luke.drury@red-gate.com>
2015-03-18 10:32:43 +0000

    Green: Empty stack DarkQueenSnap returns false

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/DarkQueenSnap.cs
@@ -0,0 +1,10 @@
+﻿namespace CelticEgyptianRatscrewKata
+{
+    public class DarkQueenSnap: ISnapRule
+    {
+        public bool IsSnap(Stack stack)
+        {
+            return false;
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapDarkQueenTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class SnapDarkQueenTests
+    {
+        [Test]
+        public void EmptyStackIsNotAValidSnap()
+        {
+            var snapRule = new DarkQueenSnap();
+            var cards = new List<Card>();
+            Stack emptyStack = new Stack(cards);
+
+            var result = snapRule.IsSnap(emptyStack);
+
+            Assert.IsFalse(result);
+        } 
+    }
+}
