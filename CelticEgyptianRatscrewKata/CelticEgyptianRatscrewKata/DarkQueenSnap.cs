371700815467aa231342bd3504f96abcd050ec88
Luke Drury <luke.drury@red-gate.com>
2015-03-18 10:41:37 +0000

    Green: implement dark queen snap rule

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/DarkQueenSnap.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/DarkQueenSnap.cs
@@ -1,9 +1,19 @@
-﻿namespace CelticEgyptianRatscrewKata
+﻿using System.Linq;
+
+namespace CelticEgyptianRatscrewKata
 {
     public class DarkQueenSnap: ISnapRule
     {
+        private static readonly Card s_DarkQueen = new Card(Suit.Spades, Rank.Queen);
+
         public bool IsSnap(Stack stack)
         {
+            if (stack.Any())
+            {
+                var topCardOfStack = stack.First();
+                return topCardOfStack.Equals(s_DarkQueen);
+            }
+
             return false;
         }
     }
