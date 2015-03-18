4fe15b7b7ca8f39f5fa27bcf50ea7cc9ec958e07
Luke Drury <luke.drury@red-gate.com>
2015-03-18 10:28:10 +0000

    Green: test for true in middle of stack returns true

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapValidatorTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapValidatorTests.cs
@@ -54,5 +54,27 @@ namespace CelticEgyptianRatscrewKata.Tests
 
             Assert.IsFalse(result);
         }
+
+        [Test]
+        public void SnapValidatorReturnsTrueWhenSnapRuleInMiddleOfListReturnsTrue()
+        {
+            var falseSnapRuleMock = new Mock<ISnapRule>();
+            var trueSnapRuleMock = new Mock<ISnapRule>();
+            List<ISnapRule> rules = new List<ISnapRule>
+                                    {
+                                        falseSnapRuleMock.Object,
+                                        trueSnapRuleMock.Object,
+                                        falseSnapRuleMock.Object
+                                    };
+            var snapValidator = new SnapValidator(rules);
+            falseSnapRuleMock.Setup(x => x.IsSnap(It.IsAny<Stack>())).Returns(false);
+            trueSnapRuleMock.Setup(x => x.IsSnap(It.IsAny<Stack>())).Returns(true);
+            IEnumerable<Card> cards = new List<Card>();
+            Stack stack = new Stack(cards);
+            
+            var result = snapValidator.IsSnap(stack);
+
+            Assert.IsTrue(result);
+        }
     }
 }
