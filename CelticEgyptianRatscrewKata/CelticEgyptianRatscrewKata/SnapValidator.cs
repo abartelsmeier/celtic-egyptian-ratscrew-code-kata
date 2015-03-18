f88d5a954f9b03eed97afed05cedc6a4ab11d140
Luke Drury <luke.drury@red-gate.com>
2015-03-18 09:55:11 +0000

    stub out snap validator

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SnapValidator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace CelticEgyptianRatscrewKata
+{
+    public class SnapValidator: ISnapRule
+    {
+        private List<ISnapRule> m_Rules;
+
+        public SnapValidator(List<ISnapRule> rules)
+        {
+            m_Rules = rules;
+
+        }
+        
+        public bool IsSnap(Stack stack)
+        {
+           return false;
+        }
+    }
+}
