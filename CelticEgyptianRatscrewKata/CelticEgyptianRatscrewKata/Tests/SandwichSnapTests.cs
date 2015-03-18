1f2ad72c7ee22db5b698f64fa505e43a7e7b02aa
Luke Drury <luke.drury@red-gate.com>
2015-03-18 09:27:09 +0000

    Red: sandwich in stack of three cards is valid snap

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapTests.cs
@@ -15,6 +15,25 @@ namespace CelticEgyptianRatscrewKata.Tests
             var result = sandwichSnap.IsSnap(emptyStack);
 
             Assert.IsFalse(result);
-        } 
+        }
+
+        [Test]
+        public void ASandwichOfTwoCardsOfSameRankIsValidSnap()
+        {
+            var standardSnap = new SandwichSnap();
+            var cards = new List<Card>
+                        {
+                            new Card(Suit.Clubs, Rank.Ace),
+                            new Card(Suit.Clubs, Rank.Two),
+                            new Card(Suit.Diamonds, Rank.Ace)
+
+                        };
+            Stack snapStack = new Stack(cards);
+
+            var result = standardSnap.IsSnap(snapStack);
+
+            Assert.IsTrue(result);
+
+        }
     }
 }
