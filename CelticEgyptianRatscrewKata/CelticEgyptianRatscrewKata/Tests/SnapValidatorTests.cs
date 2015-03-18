98e20794b8c0971d4761e65fa04b717404ba45ae
Luke Drury <luke.drury@red-gate.com>
2015-03-18 10:10:45 +0000

    Passing test for snap validator with no rules

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapValidatorTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class SnapValidatorTests
+    {
+        [Test]
+        public void SnapValidatorReturnsFalseWithNoSnapRules()
+        {
+            List<ISnapRule> rules = new List<ISnapRule>();
+            var snapValidator = new SnapValidator(rules);
+            
+            IEnumerable<Card> cards = new List<Card>();
+            Stack stack = new Stack(cards);
+            var result = snapValidator.IsSnap(stack);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
