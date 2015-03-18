26c87d8ccd70ece4d4112e31f714bcbd65fa70c6
Luke Drury <luke.drury@red-gate.com>
2015-03-18 09:32:56 +0000

    Green: test for SandwichSnap passed

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SandwichSnap.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SandwichSnap.cs
@@ -2,8 +2,18 @@
 {
     public class SandwichSnap
     {
-        public bool IsSnap(Stack emptyStack)
+        public bool IsSnap(Stack stack)
         {
+            Card previousCard = null;
+            Card previousPreviousCard = null;
+            
+            foreach (var card in stack)
+            {
+                if (previousPreviousCard != null && card.Rank == previousPreviousCard.Rank) return true;
+                previousPreviousCard = previousCard;
+                previousCard = card;
+            }
+
             return false;
         }
     }
