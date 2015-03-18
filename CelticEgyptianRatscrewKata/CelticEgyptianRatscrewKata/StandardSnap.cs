8ac61d354b44e45e6bc385cca63690a666262f2d
Luke Drury <luke.drury@red-gate.com>
2015-03-18 09:10:11 +0000

    Green: added snap implementation

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/StandardSnap.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/StandardSnap.cs
@@ -1,9 +1,20 @@
-﻿namespace CelticEgyptianRatscrewKata
+﻿using System.Linq;
+
+namespace CelticEgyptianRatscrewKata
 {
     public class StandardSnap
     {
         public bool IsSnap(Stack stack)
         {
+           
+            Card previousCard = null;
+
+            foreach (var card in stack)
+            {
+                if(previousCard != null && card.Rank == previousCard.Rank) return true;
+                previousCard = card;
+            }
+
             return false;
         }
     }
