78acd305de5c90d9470dbb9fc1bf48b86291b0ab
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-20 16:15:32 +0000

    Red: failed to create game instance

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    class GameTests
+    {
+        [Test]
+        public void ShouldCreateGame()
+        {
+            Assert.That(false, Is.True);
+        }
+    }
+}
