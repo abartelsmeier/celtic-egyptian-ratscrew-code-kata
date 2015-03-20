3964a041d9f26fb17a41ed4c61ac9294b00a1190
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-20 21:22:00 +0000

    Green: test to deal whole ordered deck to players gives expected number of cards in each hand

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameTests.cs
@@ -103,6 +103,31 @@ namespace CelticEgyptianRatscrewKata.Tests
             Assert.That((Dictionary<IPlayer, Cards>)game.PlayersWithHands, Is.EqualTo(expectedPlayerHands));
         }
 
+        [Test]
+        public void PassWholeDeckToGameThenDealToPlayersAndCheckEachHandHasCorrectNumberOfCards()
+        {
+            Cards deck = new Cards(WholeDeck());
+            var player1 = new Player();
+            var player2 = new Player();
+            var player3 = new Player();
+            List<Player> players = new List<Player>
+                            {
+                                player1,
+                                player2,
+                                player3
+                            };
+            Game game = new Game(players, deck);
+            int numberOfPlayers = players.Count;
+            int numberOfCardsInDeck = deck.Count();
+            
+            game.DealDeck();
+
+            Assert.That(game.PlayersWithHands[player1].Count(), Is.EqualTo(18)); // 52 / 3 = 17 (remainder 1) 
+            Assert.That(game.PlayersWithHands[player2].Count(), Is.EqualTo(17));
+            Assert.That(game.PlayersWithHands[player3].Count(), Is.EqualTo(17));
+            
+        }
+
         private static Card AceOfClubs()
         {
             return new Card(Suit.Clubs, Rank.Ace);
@@ -117,5 +142,67 @@ namespace CelticEgyptianRatscrewKata.Tests
         {
             return new Card(Suit.Clubs, Rank.Three);
         }
+        private static Cards WholeDeck()
+        {
+            return new Cards(new List<Card>
+                            {
+                                new Card(Suit.Clubs, Rank.Ace),
+                                new Card(Suit.Clubs, Rank.Two),
+                                new Card(Suit.Clubs, Rank.Three),
+                                new Card(Suit.Clubs, Rank.Four),
+                                new Card(Suit.Clubs, Rank.Five),
+                                new Card(Suit.Clubs, Rank.Six),
+                                new Card(Suit.Clubs, Rank.Seven),
+                                new Card(Suit.Clubs, Rank.Eight),
+                                new Card(Suit.Clubs, Rank.Nine),
+                                new Card(Suit.Clubs, Rank.Ten),
+                                new Card(Suit.Clubs, Rank.Jack),
+                                new Card(Suit.Clubs, Rank.Queen),
+                                new Card(Suit.Clubs, Rank.King),
+
+                                new Card(Suit.Diamonds, Rank.Ace),
+                                new Card(Suit.Diamonds, Rank.Two),
+                                new Card(Suit.Diamonds, Rank.Three),
+                                new Card(Suit.Diamonds, Rank.Four),
+                                new Card(Suit.Diamonds, Rank.Five),
+                                new Card(Suit.Diamonds, Rank.Six),
+                                new Card(Suit.Diamonds, Rank.Seven),
+                                new Card(Suit.Diamonds, Rank.Eight),
+                                new Card(Suit.Diamonds, Rank.Nine),
+                                new Card(Suit.Diamonds, Rank.Ten),
+                                new Card(Suit.Diamonds, Rank.Jack),
+                                new Card(Suit.Diamonds, Rank.Queen),
+                                new Card(Suit.Diamonds, Rank.King),
+
+                                new Card(Suit.Hearts, Rank.Ace),
+                                new Card(Suit.Hearts, Rank.Two),
+                                new Card(Suit.Hearts, Rank.Three),
+                                new Card(Suit.Hearts, Rank.Four),
+                                new Card(Suit.Hearts, Rank.Five),
+                                new Card(Suit.Hearts, Rank.Six),
+                                new Card(Suit.Hearts, Rank.Seven),
+                                new Card(Suit.Hearts, Rank.Eight),
+                                new Card(Suit.Hearts, Rank.Nine),
+                                new Card(Suit.Hearts, Rank.Ten),
+                                new Card(Suit.Hearts, Rank.Jack),
+                                new Card(Suit.Hearts, Rank.Queen),
+                                new Card(Suit.Hearts, Rank.King),
+
+                                new Card(Suit.Spades, Rank.Ace),
+                                new Card(Suit.Spades, Rank.Two),
+                                new Card(Suit.Spades, Rank.Three),
+                                new Card(Suit.Spades, Rank.Four),
+                                new Card(Suit.Spades, Rank.Five),
+                                new Card(Suit.Spades, Rank.Six),
+                                new Card(Suit.Spades, Rank.Seven),
+                                new Card(Suit.Spades, Rank.Eight),
+                                new Card(Suit.Spades, Rank.Nine),
+                                new Card(Suit.Spades, Rank.Ten),
+                                new Card(Suit.Spades, Rank.Jack),
+                                new Card(Suit.Spades, Rank.Queen),
+                                new Card(Suit.Spades, Rank.King)
+                            });
+        }
+        
     }
 }
