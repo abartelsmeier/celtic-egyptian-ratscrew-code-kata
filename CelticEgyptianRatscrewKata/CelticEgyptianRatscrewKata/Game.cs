19f11e5b60c215451818bafa6e23fc35c4f23347
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-20 20:21:02 +0000

    add dealer and shuffler to game

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CelticEgyptianRatscrewKata
 {
@@ -6,6 +7,13 @@ namespace CelticEgyptianRatscrewKata
     {
         Dictionary<IPlayer, Cards> m_PlayersWithHands;
         Cards m_Deck;
+        IDealer m_Dealer;
+        IShuffler m_Shuffler;
+
+        public IDictionary<IPlayer, Cards> PlayersWithHands
+        {
+            get { return m_PlayersWithHands; }
+        }
 
         public Game(): this(new List<IPlayer>(), new Cards(new List<Card>()))
         {
@@ -18,16 +26,33 @@ namespace CelticEgyptianRatscrewKata
         public Game(IEnumerable<IPlayer> players, Cards deck)
         {
             m_PlayersWithHands = new Dictionary<IPlayer, Cards>();
-            foreach( var player in players )
+            foreach(var player in players)
             {
                 m_PlayersWithHands.Add( player, new Cards(new List<Card>()) );
             }
             m_Deck = deck;
+            m_Dealer = new Dealer();
+            m_Shuffler = new Shuffler();
         }
-
+               
         public void AddPlayer(IPlayer player)
         {
             m_PlayersWithHands.Add(player,new Cards(new List<Card>()));
+        }
+
+        public void ShuffleDeck()
+        {
+            m_Deck = m_Shuffler.Shuffle(m_Deck);
+        }
+
+        public void DealDeck()
+        {
+            int numberOfPlayers = m_PlayersWithHands.Count;
+            List<Cards> hands = (List<Cards>) m_Dealer.Deal(numberOfPlayers, m_Deck);
+            for(int i = 0; i < numberOfPlayers; ++i)
+            {
+                m_PlayersWithHands[m_PlayersWithHands.ElementAt(i).Key] = hands.ElementAt(i);
+            }
         }
     }
 }
