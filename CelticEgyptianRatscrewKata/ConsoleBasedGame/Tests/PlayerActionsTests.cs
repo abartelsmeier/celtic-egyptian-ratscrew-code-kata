678bd0ac002634e44fdc465b074d84d2dfea0e4c
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-26 10:59:45 +0000

    Red: create PlayerActions

---
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Tests/PlayerActionsTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleBasedGame.Tests
+{
+    class PlayerActionsTests
+    {
+        [Test]
+        public void CreatePlayerActions()
+        {
+            PlayerActions playerActions = new PlayerActions();
+        }
+    }
+}
