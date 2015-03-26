7cb6d28a2b8a59b31f6c3d5408b6e8a78a1e3fa3
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-26 11:06:13 +0000

    Red: extend test to include add new action

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/Tests/PlayerActionsTests.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Tests/PlayerActionsTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CelticEgyptianRatscrewKata.Game;
 using NUnit.Framework;
 
 namespace ConsoleBasedGame.Tests
@@ -10,9 +11,12 @@ namespace ConsoleBasedGame.Tests
     class PlayerActionsTests
     {
         [Test]
-        public void CreatePlayerActions()
+        public void CreateAndAddPlayerActions()
         {
             PlayerActions playerActions = new PlayerActions();
+            IPlayer player = new Player("PlayerA");
+            
+            playerActions.Add('a', ActionMethod, player);
         }
     }
 
