58ca32af564c550310b2a9eb15afc558e912ef27
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-26 11:45:49 +0000

    R# separate PlayerActions

---
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/PlayerActions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.Game;
+
+namespace ConsoleBasedGame
+{
+    internal class PlayerActions
+    {
+        private IDictionary<char, Tuple<Action<IPlayer>, IPlayer>> m_PlayerActions;
+
+        public PlayerActions()
+        {
+            m_PlayerActions = new Dictionary<char, Tuple<Action<IPlayer>, IPlayer>>();
+        }
+
+        public void Add(char key, Action<IPlayer> actionMethod, IPlayer player)
+        {
+            m_PlayerActions.Add(key,new Tuple<Action<IPlayer>, IPlayer>(actionMethod,player));
+        }
+
+        public void HandleKey(char key)
+        {
+            if(m_PlayerActions.ContainsKey(key))
+                m_PlayerActions[key].Item1(m_PlayerActions[key].Item2);
+        }
+    }
+}
--- CelticEgyptianRatscrewKata/ConsoleBasedGame/Tests/PlayerActionsTests.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Tests/PlayerActionsTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CelticEgyptianRatscrewKata.Game;
@@ -40,27 +38,6 @@ namespace ConsoleBasedGame.Tests
         private void ActionMethod(IPlayer player)
         {
             m_ActionMethodHasFired = true;
-        }
-    }
-
-    internal class PlayerActions
-    {
-        private IDictionary<char, Tuple<Action<IPlayer>, IPlayer>> m_PlayerActions;
-
-        public PlayerActions()
-        {
-            m_PlayerActions = new Dictionary<char, Tuple<Action<IPlayer>, IPlayer>>();
-        }
-
-        public void Add(char key, Action<IPlayer> actionMethod, IPlayer player)
-        {
-            m_PlayerActions.Add(key,new Tuple<Action<IPlayer>, IPlayer>(actionMethod,player));
-        }
-
-        public void HandleKey(char key)
-        {
-            if(m_PlayerActions.ContainsKey(key))
-                m_PlayerActions[key].Item1(m_PlayerActions[key].Item2);
         }
     }
 }
