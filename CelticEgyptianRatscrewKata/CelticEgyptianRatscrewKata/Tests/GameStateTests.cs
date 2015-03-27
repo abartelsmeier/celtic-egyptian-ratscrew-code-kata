57c1f1f60c1386f9ab1a956ee83628de374eef61
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-27 17:25:42 +0000

    Red: require interface with notify method

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameStateTests.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    class GameStateTests
+    {
+        [Test]
+        public void MockGameStateListener()
+        {
+            var mockListener = new Mock<IGameStateListener>();
+            mockListener.Setup(x => x.Notify(It.IsAny<GameStateUpdate>())).Verifiable();
+        }
+    }
+}
