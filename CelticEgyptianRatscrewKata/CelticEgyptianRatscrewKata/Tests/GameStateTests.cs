bdb92f033576251e3a06c9c28f704a8ef206f946
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-27 17:31:17 +0000

    Green: created interface and stubbed out GameStateUpdate class to pass test

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameStateTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/GameStateTests.cs
@@ -10,6 +10,19 @@ namespace CelticEgyptianRatscrewKata.Tests
         {
             var mockListener = new Mock<IGameStateListener>();
             mockListener.Setup(x => x.Notify(It.IsAny<GameStateUpdate>())).Verifiable();
+
+            mockListener.Object.Notify(new GameStateUpdate());
+
+            mockListener.Verify();
         }
+    }
+
+    public interface IGameStateListener
+    {
+        void Notify(GameStateUpdate gameStateUpdate);
+    }
+
+    public class GameStateUpdate
+    {
     }
 }
