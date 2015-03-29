eddec1ce15944a78559640524b9d99b98c411adb
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-30 00:55:58 +0100

    Red: require PlayerSnapManager

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PlayerSnapManagerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CelticEgyptianRatscrewKata.Game;
+using Moq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    class PlayerSnapManagerTests
+    {
+        [Test]
+        public void CreatePlayerSnapManager()
+        {
+            Player playerA = new Player("PlayerA");
+            List<IPlayer> players = new List<IPlayer> { playerA };
+            var gameMock = new Mock<IGameController>();
+            gameMock.Setup(x => x.Players).Returns(players);
+            var penaltyManager = new PenaltyManager(gameMock.Object.Players);
+
+            PlayerSnapManager playerSnapManager = new PlayerSnapManager(gameMock.Object, penaltyManager);
+        }
+    }
+}
