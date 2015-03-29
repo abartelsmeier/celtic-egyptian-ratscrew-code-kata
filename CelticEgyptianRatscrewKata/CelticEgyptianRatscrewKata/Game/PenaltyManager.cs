9a302215d4ef2f468c1070796ca569f077e444cb
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-29 22:44:45 +0100

    R# separate files

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/PenaltyManager.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.Tests;
+
+namespace CelticEgyptianRatscrewKata.Game
+{
+    public class PenaltyManager
+    {
+        private readonly IDictionary<IPlayer, Penalty> _penalties;
+
+        public PenaltyManager(IEnumerable<IPlayer> players)
+        {
+            _penalties = new Dictionary<IPlayer, Penalty>();
+            foreach (var player in players)
+            {
+                _penalties.Add(player, Penalty.None);
+            }
+        }
+
+        public Penalty HasPenalty(IPlayer player)
+        {
+            if (_penalties.ContainsKey(player)) return _penalties[player];
+            return Penalty.None;
+        }
+
+        public void ImposePenalty(IPlayer player, Penalty penalty)
+        {
+            if (_penalties.ContainsKey(player)) _penalties[player] = penalty;
+        }
+
+        public void ClearPenalty(IPlayer player)
+        {
+            if (_penalties.ContainsKey(player)) _penalties[player] = Penalty.None;
+        }
+
+        public void ClearAllPenalties()
+        {
+            foreach (var player in _penalties.Keys)
+            {
+                _penalties[player] = Penalty.None;
+            }
+        }
+    }
+
+    public enum Penalty
+    {
+        None,
+        PlayedOutOfTurn
+    }
+}
--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PenaltyManagerTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PenaltyManagerTests.cs
@@ -43,48 +43,4 @@ namespace CelticEgyptianRatscrewKata.Tests
             Assert.That(imposedPenalty, Is.EqualTo(Penalty.PlayedOutOfTurn));
         }
     }
-
-    public enum Penalty
-    {
-        None,
-        PlayedOutOfTurn
-    }
-
-    public class PenaltyManager
-    {
-        private readonly IDictionary<IPlayer, Penalty> _penalties;
-
-        public PenaltyManager(IEnumerable<IPlayer> players)
-        {
-            _penalties = new Dictionary<IPlayer, Penalty>();
-            foreach (var player in players)
-            {
-                _penalties.Add(player, Penalty.None);
-            }
-        }
-
-        public Penalty HasPenalty(IPlayer player)
-        {
-            if (_penalties.ContainsKey(player)) return _penalties[player];
-            return Penalty.None;
-        }
-
-        public void ImposePenalty(IPlayer player, Penalty penalty)
-        {
-            if (_penalties.ContainsKey(player)) _penalties[player] = penalty;
-        }
-
-        public void ClearPenalty(IPlayer player)
-        {
-            if (_penalties.ContainsKey(player)) _penalties[player] = Penalty.None;
-        }
-
-        public void ClearAllPenalties()
-        {
-            foreach (var player in _penalties.Keys)
-            {
-                _penalties[player] = Penalty.None;
-            }
-        }
-    }
 }
