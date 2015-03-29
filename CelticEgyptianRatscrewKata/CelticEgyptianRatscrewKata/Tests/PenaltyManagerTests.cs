bac41579d39d85653ca6f390b8e62d95f1727e71
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-29 21:58:34 +0100

    Red: Create PenaltyManager

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PenaltyManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CelticEgyptianRatscrewKata.Game;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    class PenaltyManagerTests
+    {
+        [Test]
+        public void CreatePenaltyManagerInstance()
+        {
+            List<IPlayer> players = new List<IPlayer>(); 
+            PenaltyManager penaltyManager = new PenaltyManager(players);
+        }
+    }
+}
