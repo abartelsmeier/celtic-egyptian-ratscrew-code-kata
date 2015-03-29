b2f5fff183ae3fbcb84d17f23a906aaedf5b5b71
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-29 22:51:43 +0100

    Red: require PlayerTurnManager

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/PenaltyManager.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/PenaltyManager.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using CelticEgyptianRatscrewKata.Tests;
 
 namespace CelticEgyptianRatscrewKata.Game
 {
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/PlayerTurnManagerTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using CelticEgyptianRatscrewKata.Game;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    class PlayerTurnManagerTests
+    {
+        [Test]
+        public void CreatePlayerTurnManager()
+        {
+            List<IPlayer> players = new List<IPlayer>();
+            PlayerTurnManager playerTurnManager = new PlayerTurnManager(players);
+        }
+    }
+}
