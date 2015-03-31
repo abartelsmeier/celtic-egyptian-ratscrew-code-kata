6cba4f38f9f62cf634d3683bb09be782c527db5f
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-31 11:42:15 +0100

    setup game factory ready to test just CalloutSnapRule

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/RatscrewGameFactory.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Game/RatscrewGameFactory.cs
@@ -7,16 +7,20 @@ namespace CelticEgyptianRatscrewKata.Game
     {
         public IGameController Create(ILog log)
         {
+            var playerSequence = new PlayerSequence();
+            var callout = new LoggedCallout(playerSequence, log);
+            
             ISnapRule[] rules =
             {
-                new DarkQueenSnapRule(),
-                new SandwichSnapRule(),
-                new StandardSnapRule(),
+                //new DarkQueenSnapRule(),
+                //new SandwichSnapRule(),
+                //new StandardSnapRule(),
+                new CallOutSnapRule(callout)
             };
 
             var penalties = new Penalties();
             var loggedPenalties = new LoggedPenalties(penalties, log);
-            var gameController = new GameController(new GameState(), new SnapValidator(rules), new Dealer(), new Shuffler(), loggedPenalties, new PlayerSequence());
+            var gameController = new GameController(new GameState(), new SnapValidator(rules), new Dealer(), new Shuffler(), loggedPenalties, callout);
             return new LoggedGameController(gameController, log);
         }
     }
