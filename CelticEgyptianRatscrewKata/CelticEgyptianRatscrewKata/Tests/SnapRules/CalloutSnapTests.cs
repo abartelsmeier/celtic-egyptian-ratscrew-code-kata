86754893f7637f039fa34f16db98d53e0c2d3169
Andy Bartelsmeier <abartelsmeier@gmail.com>
2015-03-31 10:47:54 +0100

    Red: require new CalloutSnapRule

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapRules/CalloutSnapTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests.SnapRules
+{
+    class CalloutSnapTests
+    {
+        [Test]
+        public void ReturnsFalseOnEmptyStack()
+        {
+            var snapRule = new CallOutSnapRule();
+
+            bool result = snapRule.IsValidSnap(Cards.Empty());
+
+            Assert.IsFalse(result);
+        }
+    }
+}
